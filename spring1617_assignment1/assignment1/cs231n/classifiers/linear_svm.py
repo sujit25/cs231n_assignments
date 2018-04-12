df37233372f366a9bbfac93a27f257bd2711eef0
sujit25 <technosujit25@gmail.com>
2018-04-12 09:56:33 +0530

    completed softmax gradient(vector & naive), svm loss (vectorized and naive)

--- spring1617_assignment1/assignment1/cs231n/classifiers/linear_svm.py
+++ spring1617_assignment1/assignment1/cs231n/classifiers/linear_svm.py
@@ -4,125 +4,114 @@ from past.builtins import xrange
 
 
 def svm_loss_naive(W, X, y, reg):
-  """
-  Structured SVM loss function, naive implementation (with loops).
+    """
+    Structured SVM loss function, naive implementation (with loops).
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    dW = np.zeros(W.shape)  # initialize the gradient as zero
 
-  # compute the loss and the gradient
-  num_classes = W.shape[1]
-  num_train = X.shape[0]
-  loss = 0.0
-  for i in xrange(num_train):
-    scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
-    for j in xrange(num_classes):
-      if j == y[i]:     # skip for correct class
-        continue
-      margin = scores[j] - correct_class_score + 1 # note delta = 1
-      if margin > 0:
-        loss += margin
-        # update partial derivative w.r.t to w_j
-        dW[:, j] += X[i]
+    # compute the loss and the gradient
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    loss = 0.0
+    for i in xrange(num_train):
+        scores = X[i].dot(W)     # X[i] - 1 X D, W - D X C, scores - 1 X C
+        correct_class_score = scores[y[i]]
+        for j in xrange(num_classes):
+            if j == y[i]:  # skip for correct class
+                continue
+            margin = scores[j] - correct_class_score + 1  # note delta = 1
+            if margin > 0:
+                loss += margin
 
-        # loss when computed with w.r.t to w_y
-        dW[:, y[i]] += -1 * X[i]
+                # update partial derivative w.r.t to w_j
+                dW[:, j] += X[i]
 
-  # Right now the loss is a sum over all training examples, but we want it
-  # to be an average instead so we divide by num_train.
-  loss /= num_train
+                # loss when computed with w.r.t to w_y
+                dW[:, y[i]] += -1 * X[i]
 
-  # Add regularization to the loss.
-  loss += reg * np.sum(W * W)
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dW.                #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
 
-  return loss, dW
+    #############################################################################
+    # TODO:                                                                     #
+    # Compute the gradient of the loss function and store it dW.                #
+    # Rather that first computing the loss and then computing the derivative,   #
+    # it may be simpler to compute the derivative at the same time that the     #
+    # loss is being computed. As a result you may need to modify some of the    #
+    # code above to compute the gradient.                                       #
+    #############################################################################
+
+    dW /= num_train
+    dW += reg * W
+
+    return loss, dW
 
 
 def svm_loss_vectorized(W, X, y, reg):
-  """
-  Structured SVM loss function, vectorized implementation.
+    """
+    Structured SVM loss function, vectorized implementation.
 
-  Inputs and outputs are the same as svm_loss_naive.
-  """
-  loss = 0.0
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+    Inputs and outputs are the same as svm_loss_naive.
+    """
+    loss = 0.0
+    num_train = X.shape[0]
+    dW = np.zeros(W.shape)  # initialize the gradient as zero
 
-  # N = 500, D = 3073, C = 10
-  # W - 3073 X 10
-  # X- 500 X 3073 training images
-  # y - 500 X 1
+    # N = 500, D = 3073, C = 10
+    # W - 3073 X 10
+    # X- 500 X 3073 training images
+    # y - 500 X 1
 
-  scores = np.dot(X, W)         # X - 4000X3073, W - 3073 X 10, scores - 4000X 10
-  y_i_scores = scores[np.arange(len(scores)), y]    # y_i_scores is scores of correct classes
-  margin = np.maximum(0, scores - np.matrix(y_i_scores).T + 1)   # calculate difference from all class scores with correct class scores
-  margin[np.arange(len(scores)), y] = 0     # assign 0 for all the correct classes
-  loss += np.sum(margin, axis = 0)     # calculate row wise sum for all the margin
-  loss += reg * np.sum(W * W)       # add regularization term
-  print(np.shape(loss))
-  # correct_class_score = scores[y]  # - 4000 X 1 - stores correct class score for each image
-  # margin = (scores + 1) - scores[y] # 4000 X 10
-  # margin[y] = 0
-  # for index in range(len(margin)):
-  #     for marginal_score_index in range(len(margin[index])):
-  #       if margin[index][marginal_score_index] > 0 :
-  #         loss += margin[index][marginal_score_index]
+    scores = np.dot(X, W)  # X - 4000 X 3073, W - 3073 X 10, scores - 4000X 10
+    y_i_scores = scores[np.arange(num_train), y]  # y_i_scores is scores of correct classes
+    # print("scores shape", scores.shape)
+    # print("y_i scores shape", np.matrix(y_i_scores).shape)
+    # print("scores[0]", scores[:1, :])
+    # print("y[0] scores", np.matrix(y_i_scores).T[:1, :])
+    # print(scores[:1, :] - np.matrix(y_i_scores).T[:1, :])
+    margin = np.maximum(0, scores - np.matrix(y_i_scores).T + 1)    # calculate difference from all class scores with correct class scores
 
-  binary = margin
+    # print("margin", margin[:1,:])
+    # margin = np.maximum(0, scores - scores[y] + 1)
 
+    margin[np.arange(num_train), y] = 0  # assign 0 for all the correct classes
+    # margin[y] = 0
+    loss += np.sum(margin)  # calculate row wise sum for all the margin
+    loss += reg * np.sum(W * W)  # add regularization term
+    loss /= num_train
 
+    # correct_class_score = scores[y]  # - 4000 X 1 - stores correct class score for each image
+    # margin = (scores + 1) - scores[y] # 4000 X 10
+    # margin[y] = 0
+    # for index in range(len(margin)):
+    #     for marginal_score_index in range(len(margin[index])):
+    #       if margin[index][marginal_score_index] > 0 :
+    #         loss += margin[index][marginal_score_index]
 
+    # dW - 3073 X10, X - 500 X 3073
+    # temp - 500 X 10
+    # temp_dW = X.T * temp -  3073 X 10
+    temp = np.ones((X.shape[0], dW.shape[1]))  # 500 X 10
+    dW = np.dot(X.T, temp) # 500 X 3073
+    dW += reg * W
+    dW /= num_train
 
-
-
-
-
-
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the structured SVM loss, storing the    #
-  # result in loss.                                                           #
-  #############################################################################
-
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
-
-
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the gradient for the structured SVM     #
-  # loss, storing the result in dW.                                           #
-  #                                                                           #
-  # Hint: Instead of computing the gradient from scratch, it may be easier    #
-  # to reuse some of the intermediate values that you used to compute the     #
-  # loss.                                                                     #
-  #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
-
-  return loss, dW
+    return loss, dW
--- spring1617_assignment1/assignment1/cs231n/classifiers/softmax.py
+++ spring1617_assignment1/assignment1/cs231n/classifiers/softmax.py
@@ -4,93 +4,84 @@ from past.builtins import xrange
 
 
 def softmax_loss_naive(W, X, y, reg):
-  """
-  Softmax loss function, naive implementation (with loops)
+    """
+    Softmax loss function, naive implementation (with loops)
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
-  num_training_examples = X.shape[0]
-  num_features = X.shape[1]
-  num_classes = W.shape[1]
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+    num_training_examples = X.shape[0]
+    num_features = X.shape[1]
+    num_classes = W.shape[1]
 
-  for training_index in range(num_training_examples):
-    data_row = X[training_index]                  # f- 1 X D
-    scores = np.dot(data_row, W)                  # W- D X C, scores- 1 X C  - class scores
-    scores -= np.max(scores)                      # normalize values by substracting max values from each value
-    #correct_score = scores[y[training_index]]     # get score for correct class of this training example
-    #overall_score_exp = np.sum(np.exp(scores))    # stores sum of probabilities for each class
-    #for class_index in range(num_classes):
-    #  overall_score_exp += np.exp(scores[class_index])     # calculate score and add score for each example
-    probabilities =  np.exp(scores) / np.sum(np.exp(scores))
-    loss += (-1 * np.log(probabilities[y[training_index]])) # calculate loss by considering only probability of correct class
-    # loss += -1 * correct_score + np.log(overall_score_exp)    # add to loss
-  loss += reg * np.sum(W * W)     # add regularization term to overall loss
+    for training_index in range(num_training_examples):
+        data_row = X[training_index]  # f- 1 X D
+        scores = np.dot(data_row, W)  # W- D X C, scores- 1 X C  - class scores
+        scores -= np.max(scores)  # normalize values by substracting max values from each value
+        exp_scores = np.exp(scores)
+        probabilities = exp_scores / np.sum(exp_scores)
+        loss += (-1 * np.log(probabilities[y[training_index]]))  # calculate loss by considering only probability of correct class
+        # loss += -1 * correct_score + np.log(overall_score_exp)    # add to loss
+        # for class_index in range(num_classes):
+        #     dW[:, class_index] = (probabilities[class_index] - (y[class_index] == class_index)) * X[training_index]
+        #
+        # for feature_index in range(num_features):
+        #     for class_index in range(num_classes):
+        #         if class_index == y[training_index]:
+        #             dW[feature_index, class_index] = X.T[feature_index][training_index] * (probabilities[class_index] - 1)
+        #         else:
+        #             dW[feature_index, class_index] = X.T[feature_index][training_index] * (probabilities[class_index])
 
-  # to compute average loss, divide by no. of training examples
-  loss /= num_training_examples
+        for class_index in range(num_classes):
+            dW[:, class_index] += X[training_index] * (probabilities[class_index] - (class_index == y[training_index]))
+    loss += reg * np.sum(W * W)  # add regularization term to overall loss
 
-
-  for feature_index in range(num_features):
-    for class_index in range(num_classes):
-      if class_index == y[training_index]:
-        dW[feature_index, class_index] = S(feature_index)(1-S(class_index))
-      else:
-        dW[feature_index, class_index] = -S(class_index)S(feature_index)
-
-
-
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-  return loss, dW
+    # to compute average loss, divide by no. of training examplesddd
+    loss /= num_training_examples
+    dW += reg * W
+    dW /= num_training_examples
+    return loss, dW
 
 
 def softmax_loss_vectorized(W, X, y, reg):
-  """
-  Softmax loss function, vectorized version.
+    """
+    Softmax loss function, vectorized version.
 
-  Inputs and outputs are the same as softmax_loss_naive.
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Inputs and outputs are the same as softmax_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+    num_train = X.shape[0]
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+    scores = np.dot(X, W)  # X * W , X - N X D, W - D X C , scores - N X C
+    scores -= np.max(scores, axis=1, keepdims=True)  # mean normalization
+    #correct_scores = scores[np.arange(len(scores)), y]  # y - N X 1, correct_scores - N X 1
+    #loss += np.sum(-1 * np.log(np.exp(correct_scores) / np.sum(np.exp(scores), axis=1)))
+    exp_scores = np.exp(scores)
+    probabilities = exp_scores/np.sum(exp_scores, axis=1, keepdims=True)
+    loss += np.sum(-1 * np.log(probabilities[np.arange(num_train), y]))
+    loss += reg * np.sum(W * W)
+    loss /= num_train
 
-  scores = np.dot(X, W)  # X * W , X - N X D, W - D X C , scores - N X C
-  correct_scores = scores[np.arange(len(scores)), y]   # y - N X 1, correct_scores - N X 1
-
-  loss += np.sum(-1 * np.log(np.exp(correct_scores)/np.sum(np.exp(scores), axis=1)))
-  return loss, dW
-
+    dscores = probabilities.copy()
+    # subtract 1 for correct class
+    dscores[np.arange(num_train), y] -=1
+    dW = np.dot(X.T, dscores)
+    dW += reg * W
+    dW /= num_train
+    return loss, dW
